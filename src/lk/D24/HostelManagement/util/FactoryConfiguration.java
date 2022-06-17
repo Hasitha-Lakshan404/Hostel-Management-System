c518e4a0a7994725a193c85b6d0a0b40931efb48
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-06-17 17:13:12 +0530

    Added Factory Configuration

---
+++ src/lk/D24/HostelManagement/util/FactoryConfiguration.java
@@ -0,0 +1,43 @@
+package lk.D24.HostelManagement.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author : Hasitha Lakshan
+ * Project :Hostel_Management_System
+ * Date :6/17/2022
+ * Time :5:12 PM
+ */
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private final SessionFactory sessionFactory;
+
+
+    private FactoryConfiguration() throws IOException {
+        Configuration configuration = new Configuration();
+        Properties p = new Properties();
+        p.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
+        configuration.setProperties(p);
+
+        // configuration.addAnnotatedClass();
+
+        sessionFactory = configuration.buildSessionFactory();
+
+    }
+
+    public static FactoryConfiguration getInstance() throws IOException {
+        return (factoryConfiguration == null) ? factoryConfiguration = new FactoryConfiguration()
+                : factoryConfiguration;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+}
