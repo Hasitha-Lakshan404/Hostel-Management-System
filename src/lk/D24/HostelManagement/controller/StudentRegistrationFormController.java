d612dc216dc628b77ecdadf79685c37ef0769f81
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-06-24 23:18:06 +0530

    Added Student Register Form

---
+++ src/lk/D24/HostelManagement/controller/StudentRegistrationFormController.java
@@ -0,0 +1,11 @@
+package lk.D24.HostelManagement.controller;
+
+/**
+ * @author : Hasitha Lakshan
+ * Project :Hostel_Management_System
+ * Date :6/24/2022
+ * Time :2:50 PM
+ */
+
+public class StudentRegistrationFormController {
+}
