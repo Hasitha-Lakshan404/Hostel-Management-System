0f5d9bd9a3b4778189c0bc93387ef40d2d524304
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-06-24 13:56:23 +0530

    Create StudentForm Controller

--- src/lk/D24/HostelManagement/controller/StudentFormController.java
+++ src/lk/D24/HostelManagement/controller/StudentFormController.java
@@ -1,5 +1,13 @@
 package lk.D24.HostelManagement.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyEvent;
+
 /**
  * @author : Hasitha Lakshan
  * Project :Hostel_Management_System
@@ -8,4 +16,21 @@ package lk.D24.HostelManagement.controller;
  */
 
 public class StudentFormController {
+    public JFXTextField txtStudentId;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtTellNo;
+    public JFXDatePicker dateDOB;
+    public JFXComboBox cmbGender;
+    public TableView tblStudent;
+    public JFXButton btnAdd;
+
+    public void textFieldValidationOnAction(KeyEvent keyEvent) {
+    }
+
+    public void studentAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void StudentClearOnAction(ActionEvent actionEvent) {
+    }
 }
