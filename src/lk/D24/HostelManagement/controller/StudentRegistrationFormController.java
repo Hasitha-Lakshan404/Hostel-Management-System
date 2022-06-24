54ba9ef952fd7fb591cdb7469ccd57a1fce996f4
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-06-24 23:21:52 +0530

    Created StudentRegistrationController

--- src/lk/D24/HostelManagement/controller/StudentRegistrationFormController.java
+++ src/lk/D24/HostelManagement/controller/StudentRegistrationFormController.java
@@ -1,5 +1,12 @@
 package lk.D24.HostelManagement.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+
 /**
  * @author : Hasitha Lakshan
  * Project :Hostel_Management_System
@@ -8,4 +15,23 @@ package lk.D24.HostelManagement.controller;
  */
 
 public class StudentRegistrationFormController {
+    public JFXComboBox cmbSelectStudent;
+    public JFXComboBox cmbSelectRoom;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtAddress;
+    public JFXDatePicker dateDOB;
+    public JFXComboBox cmbGender;
+    public JFXTextField txtRegistrationId;
+    public JFXButton btnRegister;
+    public JFXTextField txtRoomType;
+    public JFXTextField txtKeyMoney;
+
+    public void textFieldValidationOnAction(KeyEvent keyEvent) {
+    }
+
+    public void RegisterOnAction(ActionEvent actionEvent) {
+    }
+
+    public void ClearOnAction(ActionEvent actionEvent) {
+    }
 }
