45c0477f0842319049af43f0bb36ac0debfeb5aa
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-06-24 13:53:24 +0530

    Added Student and Room UI

---
+++ src/lk/D24/HostelManagement/controller/RoomFormController.java
@@ -0,0 +1,11 @@
+package lk.D24.HostelManagement.controller;
+
+/**
+ * @author : Hasitha Lakshan
+ * Project :Hostel_Management_System
+ * Date :6/21/2022
+ * Time :5:03 PM
+ */
+
+public class RoomFormController {
+}
