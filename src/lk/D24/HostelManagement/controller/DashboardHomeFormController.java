103855cee9fdde1bcbdd3da8b3cf2be2db1d5592
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-06-25 12:52:12 +0530

    Create UI navigation

--- src/lk/D24/HostelManagement/controller/DashboardHomeFormController.java
+++ src/lk/D24/HostelManagement/controller/DashboardHomeFormController.java
@@ -1,10 +1,14 @@
 package lk.D24.HostelManagement.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
+
+import java.io.IOException;
 
 /**
  * @author : Hasitha Lakshan
@@ -15,7 +19,6 @@ import javafx.scene.shape.Rectangle;
 
 public class DashboardHomeFormController {
 
-    public Rectangle icnRecSide1;
     public Label lblUserShow;
     public Label lblHomeTitleShow;
     public Rectangle icnRecSide;
@@ -26,11 +29,44 @@ public class DashboardHomeFormController {
     public JFXButton btnSettings;
 
     public void iconSideMouseEnteredOnAction(MouseEvent mouseEvent) {
+
     }
 
     public void apnNameMouseExitedOnAction(MouseEvent mouseEvent) {
     }
 
-    public void buttonsClickOnAction(MouseEvent mouseEvent) {
+    public void buttonsClickOnAction(MouseEvent mouseEvent) throws IOException {
+        Object o = mouseEvent.getSource();
+
+        if (o instanceof JFXButton){
+            JFXButton button= (JFXButton) o;
+
+            if(button.getId().equals("dashBoardButton")){
+
+            }if(button.getId().equals("studentButton")){
+                setUI("StudentForm");
+
+            }if(button.getId().equals("roomButton")){
+                setUI("RoomForm");
+
+            }if(button.getId().equals("registerStudentButton")){
+                setUI("StudentRegistrationForm");
+
+            }if(button.getId().equals("reservationDetailButton")){
+
+            }if(button.getId().equals("LogoutButton")){
+
+            }
+
+        }
+
     }
+
+    public void setUI(String URI) throws IOException {
+        apnMainPane.getChildren().clear();
+        apnMainPane.getChildren().add(FXMLLoader.load(getClass().getResource("/lk/D24/HostelManagement/view/" + URI + ".fxml")));
+
+    }
+
+
 }
