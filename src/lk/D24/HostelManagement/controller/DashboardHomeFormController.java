92a2e3f7eafac296a81ef7d42e39b9a2571e179c
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-07-01 23:25:28 +0530

    Content Updated

--- src/lk/D24/HostelManagement/controller/DashboardHomeFormController.java
+++ src/lk/D24/HostelManagement/controller/DashboardHomeFormController.java
@@ -4,6 +4,9 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,12 +17,17 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 import lk.D24.HostelManagement.bo.custom.UserBO;
 import lk.D24.HostelManagement.bo.custom.impl.UserBOImpl;
 import lk.D24.HostelManagement.dto.UserDTO;
 import lk.D24.HostelManagement.entity.User;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 
 /**
  * @author : Hasitha Lakshan
@@ -53,7 +61,7 @@ public class DashboardHomeFormController {
 
     UserBO userBO=new UserBOImpl();
 
-    public void initialize(){
+    public void initialize() throws IOException {
         apnSideNamePane.setVisible(false);
         txtDownPane.setVisible(false);
 
@@ -63,6 +71,8 @@ public class DashboardHomeFormController {
         });
 
         txtUserName.setEditable(false);
+        setUI("StudentRegistrationForm");
+        setDate();
 
     }
 
@@ -81,6 +91,7 @@ public class DashboardHomeFormController {
             JFXButton button= (JFXButton) o;
 
             if(button.getId().equals("dashBoardButton")){
+                setUI("StudentRegistrationForm");
 
             }if(button.getId().equals("studentButton")){
                 setUI("StudentForm");
@@ -192,5 +203,65 @@ public class DashboardHomeFormController {
 
     public void centerPaneClickOnAction(MouseEvent mouseEvent) {
         txtDownPane.setVisible(false);
+    }
+    public void setDate() {
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            LocalDate currentDate = LocalDate.now();
+
+            //Displaying current time in 12hour format with AM/PM
+            DateFormat dateFormat = new SimpleDateFormat("hh : mm : ss aa");
+            String dateString = dateFormat.format(new Date()).toString();
+            lblTime.setText(dateString);
+
+
+
+            String month = "";
+            switch (currentDate.getMonthValue()) {
+                case 1:
+                    month = "Jan";
+                    break;
+                case 2:
+                    month = "Feb";
+                    break;
+                case 3:
+                    month = "March";
+                    break;
+                case 4:
+                    month = "April";
+                    break;
+                case 5:
+                    month = "May";
+                    break;
+                case 6:
+                    month = "June";
+                    break;
+                case 7:
+                    month = "July";
+                    break;
+                case 8:
+                    month = "August";
+                    break;
+                case 9:
+                    month = "Sep";
+                    break;
+                case 10:
+                    month = "Oct";
+                    break;
+                case 11:
+                    month = "Nov";
+                    break;
+                case 12:
+                    month = "Dec";
+                    break;
+
+            }
+            lblDate.setText(currentDate.getYear() + "-" + month + "-" + currentDate.getDayOfMonth());
+
+
+        }),
+                new KeyFrame(Duration.seconds(1))
+        );
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
     }
 }
